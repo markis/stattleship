80844b2376cbf10250f1a3d881e499dc3fe54880
Markis Taylor <m@rkis.cc>
2017-09-02 17:09:40 -0700

    PlayerParams optional parameters

--- src/interfaces/PlayerParams.ts
+++ src/interfaces/PlayerParams.ts
@@ -4,8 +4,8 @@ export interface PlayerParams extends BaseParams {
   /**
    * Team slug such as nhl-ny for the Giants
    */
-  team_id: string;
-  birth_date: string;
+  team_id?: string;
+  birth_date?: string;
   /**
    * Player slug such as nfl-tom-brady
    */
